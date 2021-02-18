0e8004b7bf3cbd69f5deab90184e50e87b860e43
ritchey0713 <ritchey0713@gmail.com>
2021-02-18 10:12:21 -0500

    add logged in and logout logics

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
 
   def create 
     @user = User.find_by(email: params["user"]["email"])
@@ -19,11 +20,24 @@ class SessionsController < ApplicationController
   end 
 
   def logged_in
-    
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      } 
+    else 
+      render json: {
+        logged_in: false
+      }
+    end 
   end 
 
   def logout
-
+    reset_session 
+    render json: {
+      status: 200,
+      logged_in: false
+    }
   end 
 
 end 
