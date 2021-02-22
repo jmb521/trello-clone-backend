9326154b304c2070339ffaf1113ffb26d389d8ab
Jennifer Pazos <jmp521@gmail.com>
2021-02-21 23:00:44 -0600

    added actions to lists_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+
+    def create
+        @list = List.create(list_params)
+        if @list.valid?
+            render json: {
+                list: @list
+            }
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def list_params
+        params.require(:list).permit(:name, :content, :board_id)
+    end
 end
