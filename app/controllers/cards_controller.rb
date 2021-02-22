77f0d7c491b16dd09d87d7252440fa2f3a24db4d
Jennifer Pazos <jmp521@gmail.com>
2021-02-21 23:00:28 -0600

    added actions to cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,47 @@
 class CardsController < ApplicationController
+    before_action :set_card, only: [:update, :destroy]
+    def index
+        
+
+    end
+    def create
+        @card = Card.create(card_params)
+        if @card.valid?
+            render json: {
+                card: @card
+            }
+        else
+            render json: {
+                errors: @card.errors
+            }
+        end
+    end
+
+    def update
+        @card.update(card_params)
+        if @card.valid?
+            render json: {
+                card: @card
+            }
+        else
+            render json: {
+                errors: @card.errors
+            }
+        end
+
+    end
+
+    def destroy
+        #do we want to use this or just remove from view?
+
+    end
+
+    private
+    def card_params
+        params.require(:card).permit(:title, :content, :due_date, :list_id)
+    end
+
+    def set_card
+        @card = Card.find_by(id: params[:id])
+    end
 end
