7503df97900ac95a66f965ff291955ca480dd5a9
Jennifer Pazos <jmp521@gmail.com>
2021-02-25 14:41:34 -0600

    nested index actions for list and card under respective parents

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
+  resources :cards, except: [:index]
+  resources :lists, except: [:index]
   resources :boards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
@@ -11,5 +11,13 @@ Rails.application.routes.draw do
   get :logged_in, to: "sessions#logged_in"
   
   root to: "static#home"
+
+  resources :boards do 
+    resources :lists, only: [:index]
+  end
+
+  resources :lists do 
+    resources :cards, only: [:index]
+  end
 
 end
