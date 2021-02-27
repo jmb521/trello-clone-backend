3ac47ae1c906a45fae3d66cfcc79059e443ceda2
Jennifer Pazos <jmp521@gmail.com>
2021-02-27 14:32:38 -0600

    made some changes based on errors when trying to fetch

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,14 +5,14 @@ class RegistrationsController < ApplicationController
       email: params["user"]["email"], 
       password: params["user"]["password"], 
       password_confirmation: params["user"]["password_confirmation"],
-      first_name: params["user"]["first_name"]
+      first_name: params["user"]["first_name"],
       last_name: params["user"]["last_name"]
     )
 
     if @user.save
       session[:user_id] = @user.id
       render json: {
-        status :created,
+        status: "created",
         user: @user
       }
     else 
